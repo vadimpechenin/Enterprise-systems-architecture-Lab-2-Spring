ba01ec6f5885bf96139d8e54c30d939e8da0cfc3
My Name <vadim.pechenin2015@gmail.com>
2020-10-04 19:14:59 +0300

    Started with https://start.spring.io/

---
+++ src/main/java/com/example/wokers_machines_l2/WokersMachinesL2Application.java
@@ -0,0 +1,13 @@
+package com.example.wokers_machines_l2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WokersMachinesL2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WokersMachinesL2Application.class, args);
+	}
+
+}
