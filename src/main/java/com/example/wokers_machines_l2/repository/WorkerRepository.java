9c2014e0c4d0eec7cfdbf72f34bd3cdb95320b54
My Name <vadim.pechenin2015@gmail.com>
2020-10-06 21:28:48 +0300

    HTML-pages deep processing

--- src/main/java/com/example/wokers_machines_l2/repository/WorkerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.wokers_machines_l2.repository;
-
-import com.example.wokers_machines_l2.entity.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-
-}
